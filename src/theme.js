37dc2b61ab985d98c02e8196a93a8f46d29bdb8e
superorange0707 <77090682+superorange0707@users.noreply.github.com>
2024-11-12 23:54:28 +0000

    Delete src directory

--- src/theme.js
+++
@@ -1,9 +0,0 @@
-export const theme = {
-  colors: {
-    primary: '#00395D',     // Barclays dark blue
-    secondary: '#00B7C7',   // Barclays light blue
-    background: '#f5f5f5',
-    white: '#ffffff',
-    text: '#333333',
-  }
-}; 
